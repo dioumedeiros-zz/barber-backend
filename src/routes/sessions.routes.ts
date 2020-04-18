5f4c15a724b7acbae4a982edc49caead8b6afd71
DiouMedeiros <diou.182@gmail.com>
2020-04-18 20:16:29 -0300

    feat: Created middleware Error control

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,19 +5,15 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticate = new AuthenticateUserService();
+  const authenticate = new AuthenticateUserService();
 
-    const { user, token } = await authenticate.execute({ email, password });
+  const { user, token } = await authenticate.execute({ email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
